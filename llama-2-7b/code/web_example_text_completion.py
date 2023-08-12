07dadebcf4c7d3cf9020db7d5e6510141ccefded
ishaansehgal99 <ishaanforthewin@gmail.com>
2023-08-11 20:22:32 -0700

    Added sliding window for context

--- llama-2-7b/code/web_example_text_completion.py
+++ llama-2-7b/code/web_example_text_completion.py
@@ -1,7 +1,33 @@
 from flask import Flask, request, jsonify
 from llama import Llama
+# from llama.tokenizer import Tokenizer
 
 app = Flask(__name__)
+
+class SlidingWindow: 
+    def __init__(self, max_seq_len, max_gen_len): 
+        self.max_seq_len = max_seq_len
+        self.max_gen_len = max_gen_len
+        self.token_history = []
+
+    def append(self, new_prompt_tokens): 
+        available_tokens = self.max_seq_len - len(new_prompt_tokens)
+        if available_tokens < 0: 
+            print("User input exceeds the maximum token length")
+        
+        # Account for tokens required for model output
+        available_tokens -= self.max_gen_len
+
+        if available_tokens > len(self.token_history):
+            self.token_history.extend(new_prompt_tokens)
+            return self.token_history
+                
+        # If the total tokens exceed the allowed limit, remove the earliest tokens
+        global generator
+        print(generator.tokenizer.decode(self.token_history[-available_tokens:]), "context")
+        self.token_history = self.token_history[-available_tokens:] + new_prompt_tokens
+        assert len(self.token_history) == available_tokens
+        return self.token_history
 
 # Default values for the generator
 default_values = {
@@ -17,6 +43,8 @@ generator = Llama.build(
     max_seq_len=default_values['max_seq_len'],
     max_batch_size=default_values['max_batch_size']
 )
+
+window = SlidingWindow(max_seq_len=default_values['max_seq_len'])
 
 @app.route('/configure', methods=['POST'])
 def configure_generator():
@@ -34,6 +62,7 @@ def configure_generator():
         max_batch_size=max_batch_size,
     )
 
+    window.max_seq_len = max_seq_len
     return jsonify(status="success"), 200
 
 @app.route('/generate', methods=['GET'])
@@ -47,8 +76,21 @@ def generate_text():
     top_p = float(request.args.get('top_p', 0.9))
     max_gen_len = int(request.args.get('max_gen_len', 64))
 
+    # Tokenize the prompt using the tokenizer 
+    tokenizer = generator.tokenizer
+    new_prompt_tokens = tokenizer.encode(prompt, bos=False, eos=False)
+
+    # Append to sliding window
+    result_window = window.append(new_prompt_tokens)
+
+    # Concatenate the prompt history
+    prompt_with_context = tokenizer.decode(result_window)
+
+    print(prompt, "prompt")
+    print(prompt_with_context, "prompt with context")
+
     results = generator.text_completion(
-        [prompt],
+        [prompt_with_context], # Note we pass context in the same prompt
         max_gen_len=max_gen_len,
         temperature=temperature,
         top_p=top_p,
@@ -57,7 +99,7 @@ def generate_text():
     if len(results) == 0:
         return jsonify(error="No results"), 404
 
-    return jsonify(generation=results[0]['generation'])
+    return jsonify(response=results[0]['generation'])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- llama-2-7b/params.md
+++ llama-2-7b/params.md
@@ -8,6 +8,8 @@ File is to explain the parameters used by llama-2
 
 -> max_seq_len: maximum sequence length for the input text. This is usually bound by the model's architecture (e.g. the maximum length the llama transformer can take at a time) so it is in range of [1, max_input_length_supported_by_model], in this case [1, 2048] (https://github.com/facebookresearch/llama/blob/main/llama/model.py#L31)
 
--> max_gen_len: maximum sequence length for the generated text. Like max_seq_len this is also usually bound by the model architecture. In this case it is in range of [1, 2048] (https://github.com/facebookresearch/llama/blob/main/llama/generation.py#L191)
+-> max_gen_len: A cap on maximum sequence length for the generated text. Like max_seq_len this is also usually bound by the model architecture. In this case it is in range of [1, 2048] (https://github.com/facebookresearch/llama/blob/main/llama/generation.py#L191) 
+
+* Important to note that max_seq_len + max_gen_len must be <= to arch limit of 2048. So for example if you provide a 2048 input you will get 0 tokens as output. 
 
 -> max_batch_size: maximum batch size for processing. This refers to the number of input example processed simaltaneously during a forward/backward pass. Larger batch sizes usually provide more accurate gradient estimate and generally lead to faster training but require more memory and can cause out-of-memory errors if set too high. In this case llama sets max_batch_size to 32 (https://github.com/facebookresearch/llama/blob/main/llama/model.py#L30)

e564177dd90b1e45fea5cb5edcc258be5f19bdb4
ishaansehgal99 <ishaanforthewin@gmail.com>
2023-08-30 01:48:25 -0700

    feat: support for multiple gpus

--- llama-2-7b-chat/code/web_example_chat_completion.py
+++ llama-2-7b-chat/code/web_example_chat_completion.py
@@ -1,25 +1,57 @@
 from flask import Flask, request, jsonify
 from llama import Llama
 import torch
+import sys
+import signal
 import os
+import torch.distributed as dist
+import argparse
+import time
+
+# Setup argparse
+parser = argparse.ArgumentParser(description="Llama API server.")
+parser.add_argument("--ckpt_dir", default="weights/", help="Checkpoint directory.")
+parser.add_argument("--tokenizer_path", default="tokenizer.model", help="Path to the tokenizer model.")
+parser.add_argument("--max_seq_len", type=int, default=128, help="Maximum sequence length.")
+parser.add_argument("--max_batch_size", type=int, default=4, help="Maximum batch size.")
+parser.add_argument("--model_parallel_size", type=int, default=int(os.environ.get("WORLD_SIZE", 1)), help="Model parallel size.")
+args = parser.parse_args()
+
+should_shutdown = False
 
 app = Flask(__name__)
 
+def build_generator(params):
+    """Build Llama generator from provided parameters."""
+    return Llama.build(**params)
+
+def broadcast_for_reconfig(new_params):
+    """Broadcasts new configurations to worker processes."""
+    dist.broadcast_object_list(["configure", new_params, None], src=0)
+
+def broadcast_for_generation(input_string, max_gen_len, temperature, top_p):
+    """Broadcasts generation parameters to worker processes."""
+    dist.broadcast_object_list(["generate", input_string, {
+        'max_gen_len': max_gen_len,
+        'temperature': temperature,
+        'top_p': top_p
+    }], src=0)
+
+def shutdown_server():
+    """Shut down the server."""
+    time.sleep(2)  # Delay for 2 seconds to ensure the response is sent
+    os.kill(os.getpid(), signal.SIGINT)
+
 # Default values for the generator
 gen_params = {
-    'ckpt_dir': 'weights/',
-    'tokenizer_path': 'tokenizer.model',
-    'max_seq_len': 128,
-    'max_batch_size': 4,
-    'model_parallel_size': int(os.environ.get("WORLD_SIZE", 1)),
+    'ckpt_dir': args.ckpt_dir,
+    'tokenizer_path': args.tokenizer_path,
+    'max_seq_len': args.max_seq_len,
+    'max_batch_size': args.max_batch_size,
+    'model_parallel_size': args.model_parallel_size,
 }
 
-generator = Llama.build(
-    ckpt_dir=gen_params['ckpt_dir'],
-    tokenizer_path=gen_params['tokenizer_path'],
-    max_seq_len=gen_params['max_seq_len'],
-    max_batch_size=gen_params['max_batch_size'],
-)
+generator = build_generator(gen_params)
 
 @app.route('/')
 def home():
@@ -35,28 +67,36 @@ def health_check():
         return "Llama model not initialized", 500
     return "Healthy", 200
 
+@app.teardown_request
+def check_shutdown(exception=None):
+    global should_shutdown
+    if should_shutdown:
+        shutdown_server()
+
 @app.route('/configure', methods=['POST'])
 def configure_generator():
     global generator
     global gen_params
 
-    new_params = {}
-    for key, value in gen_params.items():
-        new_params[key] = request.json.get(key, value)
+    new_params = { key: request.json.get(key, value) for key, value in gen_params.items() }
 
+    # Broadcasting the configuration changes
+    broadcast_for_reconfig(new_params)
+
+    # Reconfiguring the generator on master
     try:
-        generator = Llama.build(
-            ckpt_dir=new_params['ckpt_dir'],
-            tokenizer_path=new_params['tokenizer_path'],
-            max_seq_len=new_params['max_seq_len'],
-            max_batch_size=new_params['max_batch_size'],
-            # Reinitializing generator requires this additional param
-            model_parallel_size=new_params['model_parallel_size']
-        )
+        generator = build_generator(new_params)
         gen_params = new_params
-    except Exception as e: 
-        return jsonify(error="Failed invalid parameters: " + str(e)), 400
-
+    except Exception as e:
+        print("Failed to reconfigure model: " + \
+            str(e) + " without valid model, shutting down\n") 
+        # Broadcast shutdown command to worker processes
+        dist.broadcast_object_list(["shutdown", None, None], src=0)
+        # Mark Flask server for shutdown
+        global should_shutdown
+        should_shutdown = True
+        return jsonify(error="Failed to reconfigure model"), 400
+    
     return jsonify(status="success"), 200
 
 @app.route('/chat', methods=['POST'])
@@ -67,14 +107,18 @@ def chat_completion():
         return jsonify(error="Input data is required"), 400
     
     input_string = input_data.get("input_string")
-    if not input_data:
+    if not input_string:
         return jsonify(error="Input string is required"), 400
 
     parameters = data.get("parameters", {})
+    max_gen_len = parameters.get('max_gen_len', 64)
     temperature = parameters.get('temperature', 0.6)
     top_p = parameters.get('top_p', 0.9)
-    max_gen_len = parameters.get('max_gen_len', 64)
 
+    # Broadcast generation params to worker processes
+    broadcast_for_generation(input_string, max_gen_len, temperature, top_p)
+
+    # Master's own generation
     try:
         results = generator.chat_completion(
             [input_string],
@@ -95,4 +139,38 @@ def chat_completion():
     )
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    if dist.get_rank() == 0:
+        app.run(host='0.0.0.0', port=5000)
+    else:
+        # Note to enable logs to std out uncomment 
+        sys.stdout = sys.__stdout__
+        while True:
+            worker_num = dist.get_rank()
+            print(f"Worker {worker_num} ready to recieve next command")
+            config = [None] * 3  # Command and its associated data
+            dist.broadcast_object_list(config, src=0)
+            command = config[0]
+
+            if command == "generate":
+                try:
+                    input_string = config[1]
+                    parameters = config[2]
+                    generator.chat_completion(
+                        [input_string],
+                        max_gen_len=parameters.get('max_gen_len', 64),
+                        temperature=parameters.get('temperature', 0.6),
+                        top_p=parameters.get('top_p', 0.9)
+                    )
+                    print(f"Worker {worker_num} completed generation")              
+                except Exception as e:
+                    print(f"Error in generation: {str(e)}")
+                    
+            elif command == "configure":
+                try:
+                    new_params = config[1]
+                    generator = build_generator(new_params)
+                    print(f"Worker {worker_num} completed reconfigure")              
+                except Exception as e:
+                    print(f"Error in reconfiguring generator: {str(e)}")
+            elif command == "shutdown":
+                sys.exit(0)

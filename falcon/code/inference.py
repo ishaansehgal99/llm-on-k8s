0180b44c8303cc22b58c7ecdfb704a5a2b435218
ishaansehgal99 <ishaanforthewin@gmail.com>
2023-09-29 17:17:11 -0700

    feat: cleaned up scripts, added results with deepspeed

--- falcon/code/inference.py
+++ falcon/code/inference.py
@@ -1,36 +1,10 @@
-import csv
-import argparse
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
-from datetime import datetime
-import time
-import uuid
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, required=True)
-    parser.add_argument("--num_nodes", type=int, required=True)
-    parser.add_argument("--num_processes", type=int, required=True)
-    parser.add_argument("--num_gpus", type=int, required=True)
-    parser.add_argument("--num_prompts", type=int, required=True)
-    parser.add_argument("--model_parallelism", type=str, required=True)
-    parser.add_argument("--data_parallelism", type=str, required=True)
-    parser.add_argument("--quantization", type=str, required=True)
-    parser.add_argument("--machine", type=str, required=True)
-    return parser.parse_args()
+model = "tiiuae/falcon-7b"
 
-args = get_args()
-
-model_id = "tiiuae/falcon-40b-instruct"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(
-    model_id,
-    device_map="auto",
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-)
-
+tokenizer = AutoTokenizer.from_pretrained(model)
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
@@ -39,47 +13,13 @@ pipeline = transformers.pipeline(
     trust_remote_code=True,
     device_map="auto",
 )
-
-with open("requests.csv", "r") as f:
-    requests = [line.strip() for line in f.readlines()]
-
-fieldnames = ["model", "num_nodes", "num_processes", "num_gpus", "num_prompts", "prompt_len", "model_parallelism", "data_parallelism", "quantization", "machine", "inference_time", "request_id", "timestamp"]
-with open("results.csv", "a", newline='') as f:
-    writer = csv.DictWriter(f, fieldnames=fieldnames)
-    writer.writeheader()
-    
-    for idx, request in enumerate(requests):
-        start_time = time.time()
-
-        sequences = pipeline(
-            request,
-            max_length=200,
-            do_sample=True,
-            top_k=10,
-            num_return_sequences=1,
-            eos_token_id=tokenizer.eos_token_id,
-        )
-
-        end_time = time.time()
-        inference_time = end_time - start_time
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        
-        result = {
-            "model": args.model,
-            "num_nodes": args.num_nodes,
-            "num_processes": args.num_processes,
-            "num_gpus": args.num_gpus,
-            "num_prompts": args.num_prompts,
-            "prompt_len": len(request),
-            "model_parallelism": args.model_parallelism,
-            "data_parallelism": args.data_parallelism,
-            "quantization": args.quantization,
-            "machine": args.machine,
-            "inference_time": inference_time,
-            "request_id": str(uuid.uuid4()), # Generate a unique UUID
-            "timestamp": timestamp
-        }
-        writer.writerow(result)
-
-        for seq in sequences:
-            print(f"Result: {seq['generated_text']}")
+sequences = pipeline(
+   "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
+    max_length=200,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
